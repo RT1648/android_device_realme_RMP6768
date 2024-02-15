5480896275c7f54430c56f8509e98d1caa6f150e
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-02-15 11:12:32 +0000

    Revert "RMP6768: audio: Uprev audio HAL to 7.1"
    This reverts commit c69e03bcbd538cb69c1a2790e77d7f693412ffd1.

--- device.mk
+++ device.mk
@@ -76,7 +76,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service-mediatek \
-    android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
